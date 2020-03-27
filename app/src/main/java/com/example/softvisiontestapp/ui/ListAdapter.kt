5626bc1bb7efb572d3c842ddfe4b40a8b3c8bfc5
Sreehari <sreehari@qburst.com>
2020-03-27 23:30:43 +0530

    Add RecyclerView for displaying data list

---
+++ app/src/main/java/com/example/softvisiontestapp/ui/ListAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.softvisiontestapp.ui
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.softvisiontestapp.R
+import com.example.softvisiontestapp.data.model.Row
+
+class ListAdapter: RecyclerView.Adapter<ListAdapter.ListItemViewHolder>() {
+    var rows = listOf<Row>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    class ListItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val titleView: TextView = itemView.findViewById(R.id.title)
+        val descriptionView: TextView = itemView.findViewById(R.id.description)
+        val imageView: ImageView = itemView.findViewById(R.id.imageView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListItemViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view = layoutInflater.inflate(R.layout.list_item_view, parent, false)
+        return ListItemViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return rows.size
+    }
+
+    override fun onBindViewHolder(holder: ListItemViewHolder, position: Int) {
+        holder.titleView.setText(rows.get(position).title)
+        holder.descriptionView.setText(rows.get(position).description)
+    }
+}
--- app/src/main/java/com/example/softvisiontestapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/softvisiontestapp/ui/MainActivity.kt
@@ -4,23 +4,35 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.softvisiontestapp.R
 import com.example.softvisiontestapp.data.model.ApiResponseData
 import com.example.softvisiontestapp.utils.InjectorUtils
 
 class MainActivity : AppCompatActivity() {
-    lateinit var viewModel: MainActivityViewModel;
+    lateinit var viewModel: MainActivityViewModel
+    val listAdapter = ListAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setUpListView()
         val factory: MainActivityViewModelFactory = InjectorUtils.provideMainActivityViewModelFactory(applicationContext)
         viewModel = ViewModelProviders.of(this, factory).get(MainActivityViewModel::class.java)
         viewModel.apiResponseData.observe(this, Observer<ApiResponseData> { apiData ->
-            apiData.rows
-            Toast.makeText(this, apiData.rows[0].description, Toast.LENGTH_LONG).show()
+            listAdapter.rows = apiData.rows
         })
+    }
+
+    private fun setUpListView() {
+        val listView: RecyclerView = findViewById(R.id.dataListView)
+        listView.layoutManager = LinearLayoutManager(
+            this,
+            LinearLayoutManager.VERTICAL,
+            false
+        )
+        listView.adapter = listAdapter
     }
 }
